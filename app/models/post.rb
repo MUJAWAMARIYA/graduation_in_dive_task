c36eaf08fe2d4bfcca905a14a89deaa9928c735c
MUJAWMARIYA <mumarg2015@mail.com>
2019-11-29 12:48:52 +0200

    deploying my graduation task on heroku

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,11 @@ class Post < ApplicationRecord
     paginates_per 2
     has_many :commentings
 
+    validates  :user_name, :country, :province, :district, :sector, :cell, :village, :contact, :gender, :ID_number, :date, :post_reason, :confirmation, presence: true
+
+    validates :ID_number, length: { is: 16 }, allow_blank: true
+   
+    validates :user_name, :country, :province, :district, :sector, :cell, :village, presence: true, format: { with: /\A[a-zA-Z'-]*\z/ }
+
+    validates :contact, :ID_number, format: { with: /\A\d+\z/, message: "allowed Numbers only " }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
          has_many :posts
          has_many :comments
 
+         validates :email, :presence => true, :uniqueness => true
+
          def admin?
           admin
          end
