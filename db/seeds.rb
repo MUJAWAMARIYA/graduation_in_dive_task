fad3e56cb73e06b7d420a603fe209c2b8db45ba5
MUJAWAMARIYA <mumarg2015@gmail.com>
2019-12-03 16:29:53 +0200

    admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,8 @@
 
 # User.create!(email: 'tetas@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
 
-User.create!(email: 'mumargo@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'true')
-User.create!(email: 'mumarg200@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'true')
-User.create!(email: 'mentorbbb2@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentorbbb3@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentorbbb4@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentorbbb5@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentorbbb6@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentorbbb7@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentorb8bb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentor9bbb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentor10bbb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentorb11bb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentor12bbb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentor13bbb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mento14rbbb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentorbb15b@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentorb16bb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentor17bbb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentor18bbb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentor19bbb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mento20rbbb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'ment21orbbb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mento22rbbb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mentorb23bb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'mento24rbbb@gmail.com', password: '1234567nn',password_confirmation: '1234567nn', admin: 'false')
-User.create!(email: 'margrate@gmail.com', password: "1111111",password_confirmation: '1111111', admin: 'true')
+User.create!(email: 'margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'true')
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
