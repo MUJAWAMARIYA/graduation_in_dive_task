d299bf366502b3716b8f2f18060de6c0457ea77b
MUJAWAMARIYA <mumarg2015@gmail.com>
2019-12-03 17:10:42 +0200

    error  correction and all functions ...

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,33 @@
 # User.create!(email: 'tetas@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
 
 User.create!(email: 'margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'true')
+User.create!(email: '1margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '2margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '3margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '4margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '5margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '6margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '7margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '8margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '9margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '10margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '11margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '12margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '13margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '14margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '15margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '16margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '17margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '18margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
 
+User.create!(email: '19margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '20margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '21margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '22margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '24margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '26margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: '77margo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
+User.create!(email: 'jmargo1@gmail.com', password: '1234567',password_confirmation: '1234567', admin: 'false')
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
