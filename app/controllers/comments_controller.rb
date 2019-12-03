f11f38a8948f6b14fed318d9d89abaae9c52dbdf
MUJAWAMARIYA <mumarg2015@gmail.com>
2019-12-03 10:43:34 +0200

    my best

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,25 +27,25 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.build(comment_params)
     # @comment = Comment.new(comment_params)
 
-    respond_to do |format|
+  
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+  redirect_to @comment, notice: 'Comment was successfully created.' 
+       
       else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+       render :new 
+       
       end
     end
   end
 
   def update
-    respond_to do |format|
+   
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
+    redirect_to @comment, notice: 'Comment was successfully updated.' 
+     
       else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        render :edit 
+       
       end
     end
   end
@@ -54,10 +54,9 @@ class CommentsController < ApplicationController
   def destroy
     
       @comment.destroy
-      respond_to do |format|
-        format.html { redirect_to comments_path }
-        format.json { head :no_content }
-      end
+    
+      redirect_to comments_path 
+  
 end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,13 +41,13 @@ class PostsController < ApplicationController
     @post = current_user.posts.build(post_params)
     # @post = Post.new(post_params)
 
-    respond_to do |format|
+   
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+         redirect_to @post, notice: 'Post was successfully created.' 
+   
       else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+       render :new 
+
       end
     end
   end
@@ -64,23 +64,20 @@ class PostsController < ApplicationController
 #   end
 
   def update
-    respond_to do |format|
+    
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+  redirect_to @post, notice: 'Post was successfully updated.' 
       else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+       render :edit 
     end
   end
 
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  
+     redirect_to posts_url, notice: 'Post was successfully destroyed.' 
+    
+ 
   end
 
   private
@@ -98,7 +95,7 @@ class PostsController < ApplicationController
       unless current_user && current_user.admin? 
         redirect_to posts_path, notice: "only admin can access"
 
-      end
+      
     
     end
 end
