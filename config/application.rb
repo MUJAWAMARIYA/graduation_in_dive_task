24764f4ae83d7933995a9d21c10e5fd6715f3b30
MUJAWAMARIYA <mumarg2015@gmail.com>
2019-12-03 20:05:32 +0200

    my  best 100% all functions

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,10 @@ module MygraduationTask
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.i18n.available_locales = [:en, :ru]
-    config.i18n.default_locale = :en
+    
+
+#     I18n.config.available_locales = :ja
+# I18n.default_locale = :ja
 
   config.generators do |g|
     g.test_framework :rspec,
